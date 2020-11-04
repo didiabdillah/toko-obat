54011d0cc86e0690e1e9786dc4ea5b19511fe4d3
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-04 09:13:47 +0700

    Halaman Tampilan Frontend Etalase

--- application/controllers/Etalase.php
+++ application/controllers/Etalase.php
@@ -5,8 +5,13 @@ class Etalase extends CI_Controller
 {
     public function index()
     {
+        $data["title"] = "Etalase Exodus Drugstore";
+
         $data["url"] = $this->uri->segment(1);
 
-        echo "Etalase";
+        $this->load->view('templates/landing_header', $data);
+        $this->load->view('templates/landing_navbar', $data);
+        $this->load->view('landing/etalase', $data);
+        $this->load->view('templates/landing_footer');
     }
 }
---
+++ application/views/landing/etalase.php
@@ -0,0 +1,187 @@
+<div class="bg-light py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mb-0"><a href="<?php echo base_url(); ?>">Exodus Drugstore</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Etalase</strong></div>
+        </div>
+    </div>
+</div>
+
+<div class="site-section">
+    <div class="container">
+
+        <!-- <div class="row">
+          <div class="col-lg-6">
+            <h3 class="mb-3 h6 text-uppercase text-black d-block">Filter by Price</h3>
+            <div id="slider-range" class="border-primary"></div>
+            <input type="text" name="text" id="amount" class="form-control border-0 pl-0 bg-white" disabled="" />
+          </div>
+          <div class="col-lg-6">
+            <h3 class="mb-3 h6 text-uppercase text-black d-block">Filter by Reference</h3>
+            <button type="button" class="btn btn-secondary btn-md dropdown-toggle px-4" id="dropdownMenuReference"
+              data-toggle="dropdown">Reference</button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuReference">
+              <a class="dropdown-item" href="#">Relevance</a>
+              <a class="dropdown-item" href="#">Name, A to Z</a>
+              <a class="dropdown-item" href="#">Name, Z to A</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Price, low to high</a>
+              <a class="dropdown-item" href="#">Price, high to low</a>
+            </div>
+          </div>
+        </div> -->
+
+        <div class="row">
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <!-- <span class="tag">Sale</span> -->
+                <a href="shop-single.html"> <img src="images/tolakangin.jpeg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Tolak Angin</a></h3>
+                <p class="price">30.000</p>
+            </div>
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <a href="shop-single.html"> <img src="images/promag.jpeg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Promag</a></h3>
+                <p class="price">15.000</p>
+            </div>
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <a href="shop-single.html"> <img src="images/imboost.jpeg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Imboost Force</a></h3>
+                <p class="price">40.000</p>
+            </div>
+
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+
+                <a href="shop-single.html"> <img src="images/paracetamol.jpeg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Paracetamol</a></h3>
+                <p class="price">15.000</p>
+            </div>
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <a href="shop-single.html"> <img src="images/betadine.jpg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Betadine</a></h3>
+                <p class="price">20.000</p>
+            </div>
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <!-- <span class="tag">Sale</span> -->
+                <a href="shop-single.html"> <img src="images/komix.jpg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Komix Herbal</a></h3>
+                <p class="price">20.000</p>
+            </div>
+
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <!-- <span class="tag">Sale</span> -->
+                <a href="shop-single.html"> <img src="images/hansaplast.jpg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Hansaplast</a></h3>
+                <p class="price">8000</p>
+            </div>
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <a href="shop-single.html"> <img src="images/minyak.jpg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Minyak Kayu Putih</a></h3>
+                <p class="price">12.000</p>
+            </div>
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <a href="shop-single.html"> <img src="images/ester.jpg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Ester C</a></h3>
+                <p class="price">25.000</p>
+            </div>
+
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+
+                <a href="shop-single.html"> <img src="images/panadol.jpg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Panadol</a></h3>
+                <p class="price">13.000</p>
+            </div>
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <a href="shop-single.html"> <img src="images/salonpas.jpg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Salonpas</a></h3>
+                <p class="price">7000</p>
+            </div>
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+                <!-- <span class="tag">Sale</span> -->
+                <a href="shop-single.html"> <img src="images/strepsils.jpg" alt="Image" style="width: 320px; height: 330px"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Strepsils</a></h3>
+                <p class="price">38.000</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-12 text-center">
+                <div class="site-block-27">
+                    <ul>
+                        <li><a href="#">&lt;</a></li>
+                        <li class="active"><span>1</span></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li><a href="#">&gt;</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- <div class="site-section bg-secondary bg-image" style="background-image: url('images/bg_2.jpg');">
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <a href="#" class="banner-1 h-100 d-flex" style="background-image: url('images/bg_1.jpg');">
+              <div class="banner-1-inner align-self-center">
+                <h2>Pharma Products</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex ad minus rem odio voluptatem.
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <a href="#" class="banner-1 h-100 d-flex" style="background-image: url('images/bg_2.jpg');">
+              <div class="banner-1-inner ml-auto  align-self-center">
+                <h2>Rated by Experts</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex ad minus rem odio voluptatem.
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+ -->
+
+<!-- <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
+
+            <div class="block-7">
+              <h3 class="footer-heading mb-4">About Us</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius quae reiciendis distinctio voluptates
+                sed dolorum excepturi iure eaque, aut unde.</p>
+            </div>
+
+          </div>
+          <div class="col-lg-3 mx-auto mb-5 mb-lg-0">
+            <h3 class="footer-heading mb-4">Quick Links</h3>
+            <ul class="list-unstyled">
+              <li><a href="#">Supplements</a></li>
+              <li><a href="#">Vitamins</a></li>
+              <li><a href="#">Diet &amp; Nutrition</a></li>
+              <li><a href="#">Tea &amp; Coffee</a></li>
+            </ul>
+          </div>
+
+          <div class="col-md-6 col-lg-3">
+            <div class="block-5 mb-5">
+              <h3 class="footer-heading mb-4">Contact Info</h3>
+              <ul class="list-unstyled">
+                <li class="address">203 Fake St. Mountain View, San Francisco, California, USA</li>
+                <li class="phone"><a href="tel://23923929210">+2 392 3929 210</a></li>
+                <li class="email">emailaddress@domain.com</li>
+              </ul>
+            </div>
+
+
+          </div>
+        </div> -->
+<!-- 
+      </div>
+    </footer>
+  </div> -->
