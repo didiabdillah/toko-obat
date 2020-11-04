8dec499af91bae85cbc187a0a0046002fe1dc0f9
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-04 08:33:25 +0700

    Halaman Tampilan Frontend Landing

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -5,6 +5,10 @@ class Landing extends CI_Controller
 {
     public function index()
     {
-        echo "welcome";
+        $data["title"] = "Exodus Drugstore";
+
+        // $this->load->view('templates/auth_llanding', $data);
+        $this->load->view('landing/index', $data);
+        // $this->load->view('templates/auth_landing');
     }
 }
