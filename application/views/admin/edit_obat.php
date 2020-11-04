caedb1de359bbf4e9aec07723ff9a85de8a62755
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-04 16:29:17 +0700

    edit data obat form validation warning

--- application/views/admin/edit_obat.php
+++ application/views/admin/edit_obat.php
@@ -10,6 +10,7 @@
                     <div class="col col-md-2"><label for="nama" class=" form-control-label">Nama Obat</label></div>
                     <div class="col-12 col-md-10">
                         <input type="text" id="nama" name="nama" placeholder="Nama Obat..." class="form-control" value="<?= $obat["nama"]; ?>">
+                        <?php echo form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <!-- <div class="col-12 col-md-10">
                         <span class="help-block">Enter Nama Obat</span>
@@ -19,6 +20,7 @@
                     <div class="col col-md-2"><label for="harga" class=" form-control-label">Harga Obat</label></div>
                     <div class="col-12 col-md-10">
                         <input type="text" id="harga" name="harga" placeholder="Harga Obat..." class="form-control" value="<?= $obat["harga"]; ?>">
+                        <?php echo form_error('harga', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <!-- <div class="col-12 col-md-10">
                         <span class="help-block">Enter Nama Obat</span>
@@ -28,6 +30,7 @@
                     <div class="col col-md-2"><label for="banyak" class=" form-control-label">Banyak Stock</label></div>
                     <div class="col-12 col-md-10">
                         <input type="text" id="banyak" name="banyak" placeholder="Banyak Stock Obat..." class="form-control" value="<?= $obat["stock"]; ?>">
+                        <?php echo form_error('banyak', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <!-- <div class="col-12 col-md-10">
                         <span class="help-block">Enter Nama Obat</span>
@@ -37,6 +40,7 @@
                     <div class="col col-md-2"><label for="desc" class=" form-control-label">Deskripsi</label></div>
                     <div class="col-12 col-md-10">
                         <textarea id="desc" name="desc" placeholder="Deskripsi..." class="form-control"><?= $obat["deskripsi"]; ?></textarea>
+                        <?php echo form_error('desc', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <!-- <div class="col-12 col-md-10">
                         <span class="help-block">Enter Nama Obat</span>
