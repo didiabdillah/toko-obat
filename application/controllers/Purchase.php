b2470710ec108debf718e3a2b7c7e6aec59a6bb7
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-05 14:35:42 +0700

    membuat frontend cart

--- application/controllers/Purchase.php
+++ application/controllers/Purchase.php
@@ -9,11 +9,19 @@ class Purchase extends CI_Controller
         $this->load->model('Purchase_model');
     }
 
-    public function Cart()
+    public function cart()
     {
+        $data["title"] = "Chart Exodus Drugstore";
+        $data["url"] = $this->uri->segment(1);
+        // $data["obat"] = $this->Obat_model->get();
+
+        $this->load->view('templates/landing_header', $data);
+        $this->load->view('templates/landing_navbar', $data);
+        $this->load->view('purchase/cart');
+        $this->load->view('templates/landing_footer');
     }
 
-    public function Checkout()
+    public function checkout()
     {
     }
 }
--- application/models/Purchase_model.php
+++ application/models/Purchase_model.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Purchase_model extends CI_Controller
+class Purchase_model extends CI_Model
 {
 }
