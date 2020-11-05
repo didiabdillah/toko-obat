2ad6477db711a8d87fa7b42816b905ffc2cc4547
egisofyan <egisofyanharyadi@gmail.com>
2020-11-05 10:48:37 +0700

    mengubah view tentang dan mengambah file gambar

--- application/views/landing/tentang.php
+++ application/views/landing/tentang.php
@@ -4,8 +4,8 @@
             <div class="col-md-6">
                 <div class="block-16">
                     <figure>
-                        <img src="<?php echo base_url('assets/img/'); ?>bg_1.jpg" alt="Image placeholder" class="img-fluid rounded">
-                        <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span class="icon-play"></span></a>
+                        <img src="<?php echo base_url('assets/img/'); ?>sehat.png" alt="Image placeholder" class="img-fluid rounded">
+                        
 
                     </figure>
                 </div>
@@ -15,13 +15,9 @@
 
 
                 <div class="site-section-heading pt-3 mb-4">
-                    <h2 class="text-black">How We Started</h2>
+                    <h2 class="text-black">Tentang Exodus Drugstore</h2>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius repellat, dicta at laboriosam, nemo
-                    exercitationem itaque eveniet architecto cumque, deleniti commodi molestias repellendus quos sequi hic fugiat
-                    asperiores illum. Atque, in, fuga excepturi corrupti error corporis aliquam unde nostrum quas.</p>
-                <p>Accusantium dolor ratione maiores est deleniti nihil? Dignissimos est, sunt nulla illum autem in, quibusdam
-                    cumque recusandae, laudantium minima repellendus.</p>
+                <p>PT Kimia Farma adalah perusahaan industri farmasi pertama di Indonesia yang didirikan oleh Pemerintah Hindia Belanda tahun 1817. Nama perusahaan ini pada awalnya adalah NV Chemicalien Handle Rathkamp & Co. Berdasarkan kebijaksanaan nasionalisasi atas eks perusahaan Belanda di masa awal kemerdekaan, pada tahun 1958, Pemerintah Republik Indonesia melakukan peleburan sejumlah perusahaan farmasi menjadi PNF (Perusahaan Negara Farmasi) Bhinneka Kimia Farma. Kemudian pada tanggal 16 Agustus 1971, bentuk badan hukum PNF diubah menjadi Perseroan Terbatas, sehingga nama perusahaan berubah menjadi PT Kimia Farma (Persero).</p>
 
             </div>
         </div>
@@ -36,9 +32,8 @@
             <div class="col-md-6 order-md-2">
                 <div class="block-16">
                     <figure>
-                        <img src="<?php echo base_url('assets/img/'); ?>hero_1.jpg" alt="Image placeholder" class="img-fluid rounded">
-                        <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span class="icon-play"></span></a>
-
+                        <img src="<?php echo base_url('assets/img/'); ?>obat.jpg" alt="Image placeholder" class="img-fluid rounded">
+                        
                     </figure>
                 </div>
             </div>
@@ -46,13 +41,14 @@
 
 
                 <div class="site-section-heading pt-3 mb-4">
-                    <h2 class="text-black">We Are Trusted Company</h2>
+                    <h2 class="text-black">VISI DAN MISI</h2>
                 </div>
-                <p class="text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius repellat, dicta at laboriosam, nemo
-                    exercitationem itaque eveniet architecto cumque, deleniti commodi molestias repellendus quos sequi hic fugiat
-                    asperiores illum. Atque, in, fuga excepturi corrupti error corporis aliquam unde nostrum quas.</p>
-                <p class="text-black">Accusantium dolor ratione maiores est deleniti nihil? Dignissimos est, sunt nulla illum autem in, quibusdam
-                    cumque recusandae, laudantium minima repellendus.</p>
+                <p class="text-black">VISI</p>
+                <p class="text-black">Menjadi perusahaan Healthcare pilihan utama yang terintegrasi dan menghasilkan nilai yang berkesinambungan.</p>
+                <p class="text-black">MISI</p>
+                <p class="text-black">1. Melakukan aktivitas usaha di bidang-bidang industri kimia dan farmasi, perdagangan dan jaringan distribusi, ritel farmasi dan layanan kesehatan serta optimalisasi aset.</p>
+                <p class="text-black">2. Mengelola perusahaan secara Good Corporate Governance dan operational excellence didukung oleh Sumber Daya Manusia (SDM) profesional.</p>
+                <p class="text-black">3. Memberikan nilai tambah dan manfaat bagi seluruh stakeholder.</p>
 
             </div>
         </div>
@@ -212,3 +208,16 @@
       </div>
     </footer>
   </div> -->
+   <div class="row pt-5 mt-5 text-center">
+      <div class="col-md-12">
+        <p>
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          Copyright &copy;
+          <script>
+            document.write(new Date().getFullYear());
+          </script> All rights reserved | Exodus Drugstore <i aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        </p>
+      </div>
+
+    </div>
