078f3f18a50f2117134a363ac6447d6789a2e105
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-05 14:55:04 +0700

    memperbaiki cart, checkout, dan success footer

--- application/views/purchase/cart.php
+++ application/views/purchase/cart.php
@@ -206,3 +206,16 @@
       </div>
     </footer>
   </div> -->
+
+<div class="row pt-5 mt-5 text-center">
+    <div class="col-md-12">
+        <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;
+            <script>
+                document.write(new Date().getFullYear());
+            </script> All rights reserved | Exodus Drugstore <i aria-hidden="true"></i>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        </p>
+    </div>
+</div>
--- application/views/purchase/success.php
+++ application/views/purchase/success.php
@@ -62,12 +62,10 @@
                 Copyright &copy;
                 <script>
                     document.write(new Date().getFullYear());
-                </script> All rights reserved | This template is made
-                with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+                </script> All rights reserved | Exodus Drugstore <i aria-hidden="true"></i>
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             </p>
         </div>
-
     </div>
     </div>
 </footer>
