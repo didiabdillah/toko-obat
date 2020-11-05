b7f51d32ac112ed1ab4bb5627d9db612a54ebe97
egisofyan <egisofyanharyadi@gmail.com>
2020-11-05 11:12:37 +0700

    menghapus icon heart pada bagian detail_produk

--- application/views/landing/detail_product.php
+++ application/views/landing/detail_product.php
@@ -181,7 +181,7 @@
           <script>
             document.write(new Date().getFullYear());
           </script> All rights reserved | This template is made
-          with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+          with <i aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
           <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
         </p>
       </div>
