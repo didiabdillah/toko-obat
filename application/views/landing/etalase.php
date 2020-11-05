ac74f4f27f86b97f935d0373144aa4925d9e1ae3
egisofyan <egisofyanharyadi@gmail.com>
2020-11-05 11:07:50 +0700

    Menambahkan copyright di etalase

--- application/views/landing/etalase.php
+++ application/views/landing/etalase.php
@@ -125,3 +125,17 @@
       </div>
     </footer>
   </div> -->
+
+   <div class="row pt-5 mt-5 text-center">
+      <div class="col-md-12">
+        <p>
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          Copyright &copy;
+          <script>
+            document.write(new Date().getFullYear());
+          </script> All rights reserved | Exodus Drugstore <i aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        </p>
+      </div>
+
+    </div>
