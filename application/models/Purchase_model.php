60d3336fcfacda88dedc9418882a8f9bfb74b3f6
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-06 08:56:19 +0700

    create step 4 purchase add to cart system

--- application/models/Purchase_model.php
+++ application/models/Purchase_model.php
@@ -6,16 +6,29 @@ class Purchase_model extends CI_Model
     public function add_to_cart($input)
     {
         $obat = $this->db->get_where('obat', ["id" => $input["obat_id"]])->row_array();
+        $cart = $this->db->get_where('cart', ["user_id" => $input["user_id"], "id_obat" => $input["obat_id"]])->row_array();
 
-        $data = [
-            "user_id" => $input["user_id"],
-            "id_obat" => $input["obat_id"],
-            "nama_obat" => $obat["nama"],
-            "harga" => $obat["harga"],
-            "qty" => $input["qty"],
-            "total" => $obat["harga"] * $input["qty"]
-        ];
+        if ($input["user_id"] == $cart["user_id"] && $input["obat_id"] == $cart["id_obat"]) {
+            $new_qty = $input["qty"] + $cart["qty"];
+            $new_price = $obat["harga"] * $new_qty;
+            $data = [
+                "qty" => $new_qty,
+                "total" => $new_price
+            ];
 
-        $this->db->insert('cart', $data);
+            $this->db->where('id', $cart["id"]);
+            $this->db->update('cart', $data);
+        } else {
+            $data = [
+                "user_id" => $input["user_id"],
+                "id_obat" => $input["obat_id"],
+                "nama_obat" => $obat["nama"],
+                "harga" => $obat["harga"],
+                "qty" => $input["qty"],
+                "total" => $obat["harga"] * $input["qty"]
+            ];
+
+            $this->db->insert('cart', $data);
+        }
     }
 }
